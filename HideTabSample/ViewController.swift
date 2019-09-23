46cb0758631e27c9d12f050386d51ba6e5724d5f
TAKEDA Yuki <teakun@kittengine.com>
2019-09-23 15:38:09 +0900

    Initial Commit

---
+++ HideTabSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HideTabSample
+//
+//  Created by 武田悠暉 on 2019/09/23.
+//  Copyright © 2019 kittengine.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
